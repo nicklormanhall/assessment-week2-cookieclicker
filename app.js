c12beb9c0fe1c2db278105cbb8995b373e94ddca
nicklormanhall <nicklormanhall@gmail.com>
2024-03-30 19:18:51 +0000

    Added disable and enable function and reset data

--- app.js
+++ app.js
@@ -5,9 +5,10 @@
 const cookieBtn = document.getElementById("cookieBtn");
 const mixerBtn = document.getElementById("mixerBtn");
 const ovenBtn = document.getElementById("ovenBtn");
-const whiskBtn = document.getElementById("whiskBtn");
+const toolsBtn = document.getElementById("toolsBtn");
 const pressBtn = document.getElementById("pressBtn");
 const factoryBtn = document.getElementById("factoryBtn");
+const resetBtn = document.getElementById("resetBtn"); //resets coookes
 
 const upgradeItems = [
   { cost: 10, cps: 1, item: "new mixer" },
@@ -23,15 +24,16 @@ const cookiesSpan = document.getElementById("cookiesSpan");
 const cookiesPerSecondSpan = document.getElementById("cookiesPerSecondSpan");
 
 cookieBtn.addEventListener("click", buyCookie);
+resetBtn.addEventListener("click", resetData);
 
-//upgrade
+//upgrade uses an array to set the costs and cookies per second
 mixerBtn.addEventListener("click", function () {
   buyUpgrade(upgradeItems[0].cost, upgradeItems[0].cps);
 });
 ovenBtn.addEventListener("click", function () {
   buyUpgrade(upgradeItems[1].cost, upgradeItems[1].cps);
 });
-whiskBtn.addEventListener("click", function () {
+toolsBtn.addEventListener("click", function () {
   buyUpgrade(upgradeItems[2].cost, upgradeItems[2].cps);
 });
 pressBtn.addEventListener("click", function () {
@@ -41,7 +43,6 @@ factoryBtn.addEventListener("click", function () {
   buyUpgrade(upgradeItems[4].cost, upgradeItems[4].cps);
 });
 
-// Step 1: Sets the default values of 0
 const pageData = {
   cookieCount: 0,
   cookiesPerSecond: 0,
@@ -57,12 +58,13 @@ if (storageData !== null) {
 
 function buyCookie() {
   pageData.cookieCount++;
-  console.log(pageData); //test to check buyCookie is being updated
+  //   console.log(pageData); //test to check buyCookie is being updated
   updatePage();
   updateStorage();
 }
 
 function updatePage() {
+  enableButtons(); //added so the buttons are disabled before the upgrade can be made if there aren't enough cookies
   cookiesSpan.textContent = pageData.cookieCount;
   cookiesPerSecondSpan.textContent = pageData.cookiesPerSecond;
 }
@@ -78,7 +80,40 @@ function buyUpgrade(cost, cps) {
   updateStorage();
 }
 
-function enableButtons() {}
+//enables and disables the upgrade purchases if there aren't enough cookies
+// need to look at this as this is not fully working and need to
+function enableButtons() {
+  if (pageData.cookieCount < 10) {
+    mixerBtn.disabled = true;
+  }
+  if (pageData.cookieCount >= 10) {
+    mixerBtn.disabled = false;
+  }
+  if (pageData.cookieCount < 100) {
+    ovenBtn.disabled = true;
+  }
+  if (pageData.cookieCount >= 100) {
+    ovenBtn.disabled = false;
+  }
+  if (pageData.cookieCount < 1000) {
+    toolsBtn.disabled = true;
+  }
+  if (pageData.cookieCount >= 1000) {
+    toolsBtn.disabled = false;
+  }
+  if (pageData.cookieCount < 10000) {
+    pressBtn.disabled = true;
+  }
+  if (pageData.cookieCount >= 10000) {
+    pressBtn.disabled = false;
+  }
+  if (pageData.cookieCount < 100000) {
+    factoryBtn.disabled = true;
+  }
+  if (pageData.cookieCount >= 100000) {
+    factoryBtn.disabled = false;
+  }
+}
 
 setInterval(function () {
   pageData.cookieCount += pageData.cookiesPerSecond;
@@ -87,3 +122,11 @@ setInterval(function () {
   enableButtons();
   //   console.log(setInterval);
 }, 1000);
+
+function resetData() {
+  pageData.cookieCount = 0;
+  storageData.cookieCount = 0;
+  pageData.cookiesPerSecond = 0;
+  storageData.cookiesPerSecond = 0;
+  enableButtons();
+}
